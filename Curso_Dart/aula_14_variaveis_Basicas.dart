aea3256d942421ce56b19d9db7aa3c6d17396919
MauriSdL <iruam_mauri@msn.com>
2025-02-23 18:40:37 -0300

    add aula 14 e aula 15

---
+++ Curso_Dart/aula_14_variaveis_Basicas.dart
@@ -0,0 +1,23 @@
+
+// Importante o dart começa com uma funcao chamada void() {}
+//void() {
+//  conteudo dentro da funçao
+// }
+
+
+// numeros inteiros use int
+// numeros flutuantes use double
+// testo use String cobs:com s de string em mauisculo
+void main() {
+  int idade = 30;
+  String nome = 'Daniel';
+  double distancia = 24.89;
+
+  // mudar os valores das variaveis
+  idade = 31;
+  nome = 'ciolfi';
+
+  print(idade);
+  print(nome);
+  print(distancia);
+}
