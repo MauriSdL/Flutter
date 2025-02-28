075fb6f24507394094bddf6640e7907414464edc
MauriSdL <iruam_mauri@msn.com>
2025-02-28 11:15:14 -0300

    commit no 2 computador

--- Curso_Dart/aula_15_operadores_Aritimeticos.dart
+++ Curso_Dart/aula_15_operadores_Aritimeticos.dart
@@ -1,12 +1,24 @@
+// ignore_for_file: unused_local_variable
+
 void main() {
   double n1 = 10.5;
   double n2 = 47.9;
   int n3 = 3;
 
-  double r1 = n3 * (n1 + n2 - 15) * 10;
-  print(r1);
+  // double r1 = n3 * (n1 + n2 - 15) * 10;
+  // print(r1);
 
-  // incremento
+  // incremento e decremento
+  n1 += 10;
+  print(n1);
 
+  n1 -= 10;
+  print(n1);
+  
+  n1 *= 10;
+  print(n1);
+  
+  n1 /= 10;
+  print(n1);
 
 }
